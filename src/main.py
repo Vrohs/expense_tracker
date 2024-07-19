5d289bb9141f87fd5af6bb0f255d1e1ca22b6b96
Vrohs <info.rohsgaming536@gmail.com>
2024-07-20 01:24:17 +0530

    chore: Refactor CSV file initialization in d_base.py and main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import csv
+from datetime import datetime
+
+
+
+class CSV:
+    CSV_FILE = "finance data.csv"
+
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=["Date", " Amount", " Category", " Description"])
+            df.to_csv(cls.CSV_FILE, index = False)
+
+CSV.initialize_csv()
