3c33fa044cd5c2ccbff10b4c87421591a5b2c715
Vrohs <info.rohsgaming536@gmail.com>
2024-07-21 23:52:10 +0530

    add get_amount feature in entry.py

--- src/entry.py
+++ src/entry.py
@@ -1,22 +1,32 @@
 from datetime import datetime
 
+d_format = "%d-%m-%Y"
+
 def get_date(prompt, allow_default=False):
     date_str = input(prompt)
     if allow_default and not date_str:
-        return datetime.today().strftime("%d-%m-%Y")
+        return datetime.today().strftime(d_format)
     
     try:
-        valid_date = datetime.strptime(date_str, "%d-%m-%Y")
-        return valid_date.strftime("%d-%m-%Y")
+        valid_date = datetime.strptime(date_str, d_format)
+        return valid_date.strftime(d_format)
     except ValueError:
         print("Invalid input format, pls. enter in dd-mm-yyyy")
         return get_date(prompt, allow_default)
 
 def get_amount():
-    pass
+    try:
+        amount = float(input("Enter the amount: "))
+        if amount <= 0:
+            raise ValueError("Enter valid amount")
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount() 
 
 def get_category():
     pass
+
 
 def get_description():
     pass
